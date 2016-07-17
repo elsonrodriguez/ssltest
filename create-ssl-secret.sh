7fc3802babef86fb98964238dfc708b732a0d363
Elson Rodriguez <elson.rodriguez@gmail.com>
2016-07-17 14:08:10 -0700

    Update create-ssl-secret.sh

--- create-ssl-secret.sh
+++ create-ssl-secret.sh
@@ -1,3 +1,3 @@
 openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.pem -days 3650
+openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
 
